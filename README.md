b736eb75f6961ec6d10121eee3f4842ab4e905dc
Harish K Chandramohan <harishkumarchandra@gmail.com>
2021-03-04 18:59:42 -0500

    add

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# martian-market
+# MartianMarket
+
+## Summary
+
+This application is an online auction system for the Martian Land Foundation to auction available resources on Mars.
+
+### Demo App
+
+Click [here](build/frontend/index.html) to launch the MartianMarket application.
---
+++ contracts/MartianMarket.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.6.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './MartianAuction.sol';
+
+contract MartianMarket is ERC721, Ownable {
+    constructor() ERC721("MartianMarket", "MARS") public {}
+
+    // cast a payable address for the Martian Development Foundation to be the beneficiary in the auction
+    // this contract is designed to have the owner of this contract (foundation) to pay for most of the function calls
+    // (all but bid and withdraw)
+    address payable foundationAddress = address(uint160(owner()));
+
+    mapping(uint => MartianAuction) public auctions;
+
+    modifier landRegistered(uint tokenId) {
+        require(_exists(tokenId), "Land not registered!");
+        _;
+    }
+
+    function registerLand(string memory tokenURI) public payable onlyOwner {
+        uint _id = totalSupply();
+        _mint(msg.sender, _id);
+        _setTokenURI(_id, tokenURI);
+        createAuction(_id);
+    }
+
+    function createAuction(uint tokenId) public onlyOwner {
+        // your code here...
+        auctions[tokenId] = new MartianAuction(foundationAddress);
+    }
+
+    function endAuction(uint tokenId) public onlyOwner landRegistered(tokenId) {
+        require(_exists(tokenId), "Land not registered!");
+        MartianAuction auction = getAuction(tokenId);
+        // your code here...
+        auction.auctionEnd();
+        safeTransferFrom(owner(), auction.highestBidder(), tokenId);
+    }
+
+    function getAuction(uint tokenId) public view returns(MartianAuction auction) {
+        // your code here...
+        return auctions[tokenId];
+    }
+
+    function auctionEnded(uint tokenId) public view returns(bool) {
+        // your code here...
+        MartianAuction auction = getAuction(tokenId);
+        return auction.ended();
+    }
+
+    function highestBid(uint tokenId) public view landRegistered(tokenId) returns(uint) {
+        // your code here...
+        MartianAuction auction = getAuction(tokenId);
+        return auction.highestBid();
+    }
+
+    function pendingReturn(uint tokenId, address sender) public view landRegistered(tokenId) returns(uint) {
+        // your code here...
+        MartianAuction auction = getAuction(tokenId);
+        return auction.getPendingReturns(sender);
+    }
+
+    function bid(uint tokenId) public payable landRegistered(tokenId){
+        // your code here...
+        MartianAuction auction = getAuction(tokenId);
+        auction.bid.value(msg.value)(msg.sender);
+    }
+
+}
